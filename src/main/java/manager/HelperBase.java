44380105e62144583d5c6a39dbd44c7537502509
AlekseyVereshchahin <amaverik281@gmail.com>
2023-03-15 16:59:11 +0200

    1

---
+++ src/main/java/manager/HelperBase.java
@@ -0,0 +1,12 @@
+package manager;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class HelperBase {
+    WebDriver wd;
+
+    public HelperBase(WebDriver wd) {
+        this.wd = wd;
+    }
+}
