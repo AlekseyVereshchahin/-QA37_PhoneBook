b784935fdbddfcf0f823a1a6defbec57da525e22
AlekseyVereshchahin <amaverik281@gmail.com>
2023-03-17 13:50:59 +0200

    3

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -1,9 +1,19 @@
 package tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class LoginTests extends TestBase{
     @Test
-    public void Test(){
-            }
+    public void loginSuccess(){
+        app.getHelperUser().openLoginRegistrationForm();
+        app.getHelperUser().fillLoginRegistrationForm("qwerty@mail.com","Ff12345$");
+        app.getHelperUser().submitLogin();
+
+        //time-->button signOut
+
+        Assert.assertTrue(app.getHelperUser().isLogged());
+    }
+
+
 }
